64c0c3eb9ab7fa4bd8f120d4a7f207c9a716c491
mikojimnz <mikojimnz@users.noreply.github.com>
2019-10-23 11:59:09 -0400

    Initial commit

---
+++ Assets/Scripts/PinController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinController : MonoBehaviour
+{
+
+	private bool active;
+
+	void Start ()
+	{
+		active = true;
+	}
+
+	void Update ()
+	{
+		if (transform.up.y < 0.6f && active) {
+			PlayerController.AddCount ();
+			active = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public Text CountText;
+	public static int count;
+	private float timeReset;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		timeReset = 5f;
+		SetCountText ();
+		Cursor.visible = false;
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+
+		SetCountText ();
+
+		if (Input.GetKey ("escape")) {
+			Application.Quit ();
+		}
+	}
+
+	public void SetCountText ()
+	{
+		CountText.text = "Score: " + count.ToString ();
+
+		if (count == 10) {
+			timeReset -= Time.deltaTime;
+			if (timeReset < 0)
+				SceneManager.LoadScene ("Bowling");
+		}
+	}
+
+	public static void AddCount ()
+	{
+		count++;
+	}
+}
